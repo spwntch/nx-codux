f97def056527576443d2ac99435745c14c09431e
zpydee <benji@spawntech.io>
2024-07-16 11:54:56 +0200

    rm src/_codux/**

--- src/_codux/boards/page/index.board.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { createBoard } from '@wixc3/react-board';
-import Index from '../../../../apps/site/app/page';
-
-export default createBoard({
-  name: 'Index',
-  Board: () => <Index />,
-  isSnippet: true,
-});
