42a072b08b5b2d93557545af14612a7ea628ea48
zpydee <benji@spawntech.io>
2024-07-16 23:03:42 +0200

    using tailwind prests and css varaibles for colours

--- apps/site/components/widget/widget.tsx
+++ apps/site/components/widget/widget.tsx
@@ -1,12 +1,10 @@
 'use client';
-import { Button } from '@/react-ui';
-import React from 'react';
 
 const Widget = () => {
   return (
     <div className="flex-col justify-center items-center">
-      {/* <h1 className="text-5xl text-green-600">Hello world...</h1> */}
-      <Button>Click me</Button>
+      <h1 className="text-5xl text-primary">Hello world...</h1>
+      {/* <Button>Click me</Button> */}
     </div>
   );
 };
--- apps/site/tailwind.config.js
+++ apps/site/tailwind.config.js
@@ -10,7 +10,7 @@ module.exports = {
     ),
     ...createGlobPatternsForDependencies(__dirname),
   ],
-  presets: [require('../../libs/tailwind/src/tailwind.presets')],
+  presets: [require('../../tailwind.presets')],
   theme: {
     extend: {},
   },
