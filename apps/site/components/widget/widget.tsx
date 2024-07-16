fff9d3d3312b8f47e76a4a1e0e4a1077aec47386
zpydee <benji@spawntech.io>
2024-07-17 00:37:58 +0200

    trying to render our shadcn button but styles not yet woring

--- apps/site/components/widget/widget.tsx
+++ apps/site/components/widget/widget.tsx
@@ -1,10 +1,11 @@
 'use client';
+import { Button } from '@/react-ui';
 
 const Widget = () => {
   return (
     <div className="flex-col justify-center items-center">
       <h1 className="text-5xl text-primary">Hello world...</h1>
-      {/* <Button>Click me</Button> */}
+      <Button variant="outline">Click me</Button>
     </div>
   );
 };
--- libs/react-ui/src/index.ts
+++ libs/react-ui/src/index.ts
@@ -1,1 +1,3 @@
+export * from './lib/components/shadcn-ui/badge';
+export * from './lib/components/shadcn-ui/button';
 export * from './lib/utils';
