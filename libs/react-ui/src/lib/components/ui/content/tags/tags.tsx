8229415d74cb5495d468ca61dfe81adeb1b51c91
zpydee <benji@spawntech.io>
2024-07-17 21:48:42 +0200

    adds shadcn/ui + image container

--- libs/react-ui/src/lib/components/ui/content/tags/tags.tsx
+++ libs/react-ui/src/lib/components/ui/content/tags/tags.tsx
@@ -12,12 +12,12 @@ export const Tags: React.FC<ITagsProps> = ({ tags, className }) => {
     >
       {tags.map((tag, index) => (
         <div key={index}>
-          <Badge className="bg-purple-500 text-white rounded-full px-3 py-1 text-sm whitespace-nowrap overflow-hidden text-ellipsis max-w-[150px] inline-block flex-shrink-0">
+          <Badge className="bg-primary text-white rounded-full px-3 py-1 text-sm whitespace-nowrap overflow-hidden text-ellipsis max-w-[150px] inline-block flex-shrink-0">
             {tag}
           </Badge>
         </div>
       ))}
-      <div className="absolute right-0 top-0 bottom-0 w-[50px] bg-gradient-to-l from-white to-transparent" />
+      {/* <div className="absolute right-0 top-0 bottom-0 w-[50px] bg-gradient-to-l from-white to-transparent" /> */}
     </div>
   );
 };
