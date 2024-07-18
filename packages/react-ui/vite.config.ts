c6980a2da9567a106f0cadfd536e253f43cab904
zpydee <benji@spawntech.io>
2024-07-19 00:10:39 +0200

    chore: clean up vite comments

--- packages/react-ui/vite.config.ts
+++ packages/react-ui/vite.config.ts
@@ -18,13 +18,6 @@ export default defineConfig({
     }),
   ],
 
-  // Uncomment this if you are using workers.
-  // worker: {
-  //  plugins: [ nxViteTsPaths() ],
-  // },
-
-  // Configuration for building your library.
-  // See: https://vitejs.dev/guide/build.html#library-mode
   build: {
     outDir: '../../dist/packages/react-ui',
     emptyOutDir: true,
@@ -33,16 +26,12 @@ export default defineConfig({
       transformMixedEsModules: true,
     },
     lib: {
-      // Could also be a dictionary or array of multiple entry points.
       entry: 'src/index.ts',
       name: 'react-ui',
       fileName: 'index',
-      // Change this to the formats you want to support.
-      // Don't forget to update your package.json as well.
       formats: ['es', 'cjs'],
     },
     rollupOptions: {
-      // External packages that should not be bundled into your library.
       external: ['react', 'react-dom', 'react/jsx-runtime'],
     },
   },
