32f6b092c391ff9bbec31fc4347976d74817346a
zpydee <benji@spawntech.io>
2024-07-19 16:08:08 +0200

    chore: exports dropdown-menu, navigation-menu, and sheet

--- packages/react-ui/src/lib/components/shadcn-ui/index.ts
+++ packages/react-ui/src/lib/components/shadcn-ui/index.ts
@@ -4,3 +4,7 @@ export * from './badge/badge';
 export * from './button/button';
 export * from './card/card';
 export * from './carousel/carousel';
+export * from './dropdown-menu/dropdown-menu';
+export * from './menubar/menubar';
+export * from './navigation-menu/navigation-menu';
+export * from './sheet/sheet';
