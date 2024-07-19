4bf4baf41eafa8dcdb2eb88c543758f03e24281f
zpydee <benji@spawntech.io>
2024-07-19 15:36:59 +0200

    chore: export card and carousel

--- packages/react-ui/src/lib/components/shadcn-ui/index.ts
+++ packages/react-ui/src/lib/components/shadcn-ui/index.ts
@@ -2,3 +2,5 @@ export * from './accordion/accordion';
 export * from './avatar/avatar';
 export * from './badge/badge';
 export * from './button/button';
+export * from './card/card';
+export * from './carousel/carousel';
