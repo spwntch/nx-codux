7e69eeec4e0d96010deb788fb7fc28ae40ea73ea
zpydee <benji@spawntech.io>
2024-07-23 17:50:58 +0200

    fix: omits some hero content

--- apps/site/config/home/hero.tsx
+++ apps/site/config/home/hero.tsx
@@ -14,19 +14,19 @@ export const HOME_HERO_CONTENT: IContent = {
   // },
   title: {
     content: 'Unlock Your Ideal Customers with the ICP Playbook',
-    // className: 'text-4xl font-bold text-center',
   },
   subTitle: {
     content:
       'Transform your marketing and product strategies with our comprehensive playbook.',
-    // className: 'text-xl text-center mt-4',
   },
   body: {
     content: [
-      "Are you struggling to identify and target the right customers, wasting valuable marketing resources on low-potential leads? Do you face challenges in aligning your marketing and sales teams, causing friction and inefficiencies? Perhaps you're overwhelmed by the vast amounts of data available but unsure how to leverage it effectively?",
+      // 'Are you struggling to identify and target the right customers, wasting valuable marketing resources on low-potential leads?',
+      // 'Do you face challenges in aligning your marketing and sales teams, causing friction and inefficiencies?',
+      // 'Are you struggling to convert your website visitors into customers?',
+      // "Perhaps you're overwhelmed by the vast amounts of data available but unsure how to leverage it effectively?",
       'With the ICP Playbook, you can achieve higher conversion rates, improve team alignment, and see faster, data-driven results. Our comprehensive playbook provides the tools and strategies you need to transform your marketing efforts and reach your ideal customers efficiently.',
     ],
-    // className: 'text-lg text-center mt-6',
   },
   bullets: [
     {
@@ -46,11 +46,7 @@ export const HOME_HERO_CONTENT: IContent = {
     },
   ],
   ctas: [
-    { label: 'UNLOCK PLAYBOOK RESULTS', variant: { size: 'xl' } },
-    { label: 'CONTACT US', variant: { size: 'xl', variant: 'ghost' } },
+    { label: 'UNLOCK PLAYBOOK RESULTS', variant: { size: '2xl' } },
+    { label: 'CONTACT US', variant: { size: '2xl', variant: 'ghost' } },
   ],
-  // tags: {
-  //   content: ['Get Started Today', 'Learn More'],
-  //   className: 'flex justify-center space-x-4 mt-8',
-  // },
 };
