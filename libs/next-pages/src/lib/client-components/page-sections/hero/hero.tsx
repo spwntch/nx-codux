2ae2223c3b55a91e38355b60368d2235b7c72311
zpydee <benji@spawntech.io>
2024-07-23 17:14:58 +0200

    fix:set hero text to text-white

--- libs/next-pages/src/lib/client-components/page-sections/hero/hero.tsx
+++ libs/next-pages/src/lib/client-components/page-sections/hero/hero.tsx
@@ -1,17 +1,23 @@
-"use client";
+'use client';
 import { IContent, IImage, ImageContentSection } from '@spwntch/react-ui';
 import React from 'react';
 
 export interface IHeroProps {
   image: IImage;
   content: IContent;
-
 }
 
-const Hero = ({image,content}: IHeroProps) => {
-
-
-  return <ImageContentSection image={image} innerContent={content} hero hAlign='left' vAlign='bottom'/>;
+const Hero = ({ image, content }: IHeroProps) => {
+  return (
+    <ImageContentSection
+      image={image}
+      innerContent={content}
+      hero
+      hAlign="left"
+      vAlign="bottom"
+      className="text-white"
+    />
+  );
 };
 
 export default Hero;
