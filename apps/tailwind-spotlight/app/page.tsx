65504faa3b08cf784ec359c5d11f2abe64041811
zpydee <benji@spawntech.io>
2024-07-25 11:47:45 +0200

    feat: adds new boilerplate app, tailwind-spotlight

---
+++ apps/tailwind-spotlight/app/page.tsx
@@ -0,0 +1,10 @@
+export default function Index() {
+  /*
+   * Replace the elements below with your own.
+   *
+   * Note: The corresponding styles are in the ./index.tailwind file.
+   */
+  return (
+    <div className="text-4xl">hello</div>
+  );
+}
