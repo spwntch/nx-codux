8ff628f8f6dd27fc2a3b5fcfaebd3a99ed0ee795
zpydee <benji@spawntech.io>
2024-07-31 14:55:56 +0200

    feat: adds png versions of benefit images

--- apps/icp-playbook/config/home/benefits.tsx
+++ apps/icp-playbook/config/home/benefits.tsx
@@ -17,7 +17,7 @@ export const BENEFITS: IContent = {
   },
   bullets: [
     {
-      image: { src: 'images/improved-targeting.webp' },
+      image: { src: 'images/benefits/increased-sales.webp' },
       heading: 'Improved Targeting & Conversions',
       body: [
         'Identify and focus on high-potential customers, reducing wasted marketing spend and boosting conversion rates. With the ICP Playbook, gain precision in your targeting efforts, leading to higher lead quality, increased engagement, and more successful sales.',
@@ -34,7 +34,7 @@ export const BENEFITS: IContent = {
       ],
     },
     {
-      image: { src: 'images/data-driven-decision-making.webp' },
+      image: { src: 'images/benefits/data-driven-decisions.webp' },
       heading: 'Data-Driven Decision Making',
       body: [
         'Utilize analytics and data to refine customer profiles and improve business outcomes. The ICP Playbook empowers you with the insights needed to make informed decisions.',
@@ -51,7 +51,7 @@ export const BENEFITS: IContent = {
       ],
     },
     {
-      image: { src: 'images/team-alignment.webp' },
+      image: { src: 'images/benefits/team-alignment.webp' },
       heading: 'Team Alignment',
       body: [
         'Achieve better alignment between marketing, sales, and product teams. The ICP Playbook ensures everyone is on the same page, working towards common goals with a unified strategy.',
@@ -68,7 +68,7 @@ export const BENEFITS: IContent = {
       ],
     },
     {
-      image: { src: 'images/better-features.webp' },
+      image: { src: 'images/benefits/better-features.webp' },
       heading: 'Better Feature Identification',
       body: [
         'Empower product owners to identify and prioritize features that resonate with their perfect customers. The ICP Playbook provides the tools and insights needed to enhance product development strategies.',
@@ -85,7 +85,7 @@ export const BENEFITS: IContent = {
       ],
     },
     {
-      image: { src: 'images/professional-growth.webp' },
+      image: { src: 'images/benefits/professional-growth.webp' },
       heading: 'Professional Growth',
       body: [
         'Stay updated with the latest strategies and tools to advance your career. The ICP Playbook equips you with cutting-edge knowledge and skills to remain competitive and innovative.',
@@ -102,7 +102,7 @@ export const BENEFITS: IContent = {
       ],
     },
     {
-      image: { src: 'images/enhanced-cx-ux.webp' },
+      image: { src: 'images/benefits/enhanced-experience.webp' },
       heading: 'Enhanced CX/UX',
       body: [
         'Develop deeper insights into customer needs, leading to more personalized and engaging experiences. The ICP Playbook helps you create customer experiences that resonate and delight.',
