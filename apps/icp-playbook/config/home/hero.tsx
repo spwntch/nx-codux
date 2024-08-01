c943472cf2bdbd4b52dbad9783485143a6d70b25
zpydee <benji@spawntech.io>
2024-08-01 12:48:00 +0200

    feat: picks a image to fonuc in for hero

--- apps/icp-playbook/config/home/hero.tsx
+++ apps/icp-playbook/config/home/hero.tsx
@@ -2,7 +2,7 @@ import { IContent, IImage } from '@spwntch/react-ui';
 import { Crosshair, Users, TrendingUp } from 'lucide-react';
 
 export const HOME_HERO_IMAGE: IImage = {
-  src: '/images/home/hero/all-benefits-landscape.webp',
+  src: '/images/benefits/increased-sales.webp',
   darken: '75%',
 };
 
