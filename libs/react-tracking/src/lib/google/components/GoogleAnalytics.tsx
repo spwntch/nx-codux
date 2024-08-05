ad841cc5e918856e894a143eb5ff0270baccdf3a
zpydee <benji@spawntech.io>
2024-08-05 17:22:51 +0200

    feat: adds GoogleAnalytics component

---
+++ libs/react-tracking/src/lib/google/components/GoogleAnalytics.tsx
@@ -0,0 +1,36 @@
+'use client';
+
+import Script from 'next/script';
+
+declare global {
+  interface Window {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    gtag: any;
+  }
+}
+
+export const GoogleAnalytics = () => {
+  if (!process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS) return null;
+  return (
+    <>
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+      <Script id="google-analytics">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+ 
+          gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
+          ${
+            process.env.NEXT_PUBLIC_GOOGLE_ADS &&
+            `gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ADS}');`
+          }
+        `}
+      </Script>
+    </>
+  );
+};
+
+export default GoogleAnalytics;
