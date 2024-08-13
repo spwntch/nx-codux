7ecb507447b11a1b7a01b9bb5eda9290f3a7c3ce
zpydee <benji@spawntech.io>
2024-08-13 15:12:29 +0200

    hides nav

--- apps/journey-mapping/config/nav.ts
+++ apps/journey-mapping/config/nav.ts
@@ -1,14 +1,8 @@
 import { INavItem } from '@spwntch/react-ui';
 
 export const primaryNav: INavItem[] = [
-  // { label: 'Home', href: '/' },
-  { label: 'The Playbook', href: '#product' },
-  { label: 'FAQ', href: '#faq' },
-  { label: 'Pricing', href: '#pricing' },
-  { label: 'About Us', href: '#about' },
-  // {
-  //   label: 'Free Resources',
-  //   href: '#resources',
-  // },
-  // { label: 'Get Started', href: '#get-started' },
+  // { label: 'The Playbook', href: '#product' },
+  // { label: 'FAQ', href: '#faq' },
+  // { label: 'Pricing', href: '#pricing' },
+  // { label: 'About Us', href: '#about' },
 ];
