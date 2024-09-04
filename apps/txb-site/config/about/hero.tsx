305ba24f9b78197cd0888f857f481e3cf54b303d
zpydee <benji@spawntech.io>
2024-09-04 15:52:04 +0200

    better hero consistency

--- apps/txb-site/config/about/hero.tsx
+++ apps/txb-site/config/about/hero.tsx
@@ -8,17 +8,17 @@ export const ABOUT_HERO_IMAGE: IImage = {
 export const ABOUT_HERO_CONTENT: IContent = {
   title: {
     content: 'Welcome to The Experience Business',
-    className: 'text-5xl font-bold text-center',
+    className: 'max-w-5xl'
   },
   subheading: {
     content: 'Innovating Customer Experiences through AI and Technology',
-    className: 'text-2xl text-center mt-4',
+    className: 'max-w-lg mx-auto'
   },
   body: {
     content: [
       'At The Experience Business (TXB), we leverage cutting-edge technology and artificial intelligence to make advanced UX, lean product development, and insights management accessible to businesses of all sizes. Our mission is to transform how companies engage with their customers, driving innovation and delivering tangible results.',
       'Join us on a journey to elevate your business with strategies that are as intelligent as they are impactful, ensuring that every customer interaction is a step towards lasting success.',
     ],
-    className: 'max-w-4xl mx-auto text-center mt-6',
+    className: 'max-w-3xl text-center mx-auto',
   },
 };
--- apps/txb-site/config/contact/hero.tsx
+++ apps/txb-site/config/contact/hero.tsx
@@ -8,17 +8,17 @@ export const CONTACT_HERO_IMAGE: IImage = {
 export const CONTACT_HERO_CONTENT: IContent = {
   title: {
     content: 'Get in Touch with The Experience Business',
-    className: 'text-5xl font-bold text-center',
+    className: 'max-w-3xl',
   },
   subheading: {
     content: 'We’re Here to Help You Innovate and Grow',
-    className: 'text-2xl text-center mt-4',
+    className: '',
   },
   body: {
     content: [
       'Whether you have questions about our services, need support, or want to explore how we can collaborate, our team is ready to assist you. At The Experience Business, we value strong partnerships and are committed to helping you achieve your business goals.',
       'Reach out to us today and discover how we can work together to create exceptional customer experiences and drive lasting success.',
     ],
-    className: 'max-w-4xl mx-auto text-center mt-6',
+    className: 'max-w-3xl text-center mx-auto',
   },
 };
